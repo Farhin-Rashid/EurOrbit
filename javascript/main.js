78a6bd1d9872d5e1eef1c9d3e87cb47d0a50b846
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2024-07-26 13:38:47 -0500

    populated dropdown menu

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,8 @@ fetch("city_coordinates.csv")
     const result = [];
     const headers = lines[0].split(",");
 
+    headers[headers.length - 1] = headers[headers.length - 1].replace("\r", "");
+
     for (let i = 1; i < lines.length; i++) {
       const obj = {};
       const currentLine = lines[i].split(",");
@@ -16,33 +18,19 @@ fetch("city_coordinates.csv")
       result.push(obj);
     }
 
-    console.log(result);
+    // Add the data to the dropdown menu
+    const dropMenu = document.getElementById("drop-menu"); // Get the dropdown menu element
+
+    result.forEach(item => {
+      const li = document.createElement("li"); // Create a new list item element
+      li.classList.add("dropdown-item")
+
+      // Set the text content of the list item to display the data
+      li.textContent = `${item.city}, ${item.country}`;
+
+      dropMenu.appendChild(li); // Append the list item to the dropdown menu
+    });
   })
   .catch(error => {
     console.error(error);
   });
-
-const xhr = new XMLHttpRequest();
-xhr.open("GET", "city_coordinates.csv", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const csv = xhr.responseText;
-    const lines = csv.split("\n");
-    const result = [];
-    const headers = lines[0].split(",");
-
-    for (let i = 1; i < lines.length; i++) {
-      const obj = {};
-      const currentLine = lines[i].split(",");
-
-      for (let j = 0; j < headers.length; j++) {
-        obj[headers[j]] = currentLine[j];
-      }
-
-      result.push(obj);
-    }
-
-    console.log(result);
-  }
-};
-xhr.send();
